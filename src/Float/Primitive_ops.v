bfadd2d3ccbd1d9910c3829a5bbcb6840abc9b19
Pierre Roux <pierre.roux@onera.fr>
2019-11-06 14:53:05 +0100

    Fix fromZ

--- src/Float/Primitive_ops.v
+++ src/Float/Primitive_ops.v
@@ -51,11 +51,25 @@ Definition one := one.
 Definition nan := nan.
 
 Definition fromZ_default default x :=
+  let s := Z.ltb x 0 in
+  let x := Z.abs x in
   let f := of_int63 (Int63.of_Z x) in
   let (m, e) := frshiftexp f in
   let m := normfr_mantissa m in
-  let i := (bigZ_of_int m * 2 ^ (bigZ_of_int e - BigZ.of_Z FloatOps.shift))%bigZ in
-  if (BigZ.of_Z x =? i)%bigZ then f else default.
+  let m := bigZ_of_int m in
+  let e := bigZ_of_int e in
+  match (e - BigZ.of_Z (FloatOps.shift + FloatOps.prec))%bigZ with
+  | (BigZ.Pos _) as e' =>
+    if (BigZ.of_Z x =? BigZ.shiftl m e')%bigZ then
+      if s then (-f)%float else f
+    else
+      default
+  | (BigZ.Neg _) as e' =>
+    if (BigZ.shiftl (BigZ.of_Z x) (-e') =? m)%bigZ then
+      if s then (-f)%float else f
+    else
+      default
+  end.
 
 Definition fromZ := fromZ_default nan.
 
